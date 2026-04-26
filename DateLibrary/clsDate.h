ebd73b2a9731c14972d3af71480af7e7692ef0a7
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-04-26 17:23:50 +0800

    update

--- DateLibrary/clsDate.h
+++ DateLibrary/clsDate.h
@@ -1,11 +1,449 @@
 #pragma once
-#include<iostream>
-#include<string>
-#include"clsString.h"
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class clsDate
 {
+private:
 
+	static bool _IsLeapYear(short Year)
+	{
+		return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+	}
+
+	static short _NumberOfDaysInAMonth(short Month, short Year)
+	{
+		short Days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		if (Month == 2 && _IsLeapYear(Year))
+			return 29;
+
+		return Days[Month - 1];
+	}
+
+	static short _DayOfWeekOrder(short Day, short Month, short Year)
+	{
+		short a = (14 - Month) / 12;
+		short y = Year - a;
+		short m = Month + 12 * a - 2;
+
+		return (Day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+	}
+
+	static bool _IsDate1BeforeDate2(clsDate Date1, clsDate Date2)
+	{
+		if (Date1.Year != Date2.Year)
+			return Date1.Year < Date2.Year;
+
+		if (Date1.Month != Date2.Month)
+			return Date1.Month < Date2.Month;
+
+		return Date1.Day < Date2.Day;
+	}
+
+	static bool _IsDate1EqualDate2(clsDate Date1, clsDate Date2)
+	{
+		return (Date1.Day == Date2.Day && Date1.Month == Date2.Month && Date1.Year == Date2.Year);
+	}
+
+	static short _GetDifferenceInDays(clsDate Date1, clsDate Date2, bool IncludeEndDay = false)
+	{
+		short Days = 0;
+		short Sign = 1;
+
+		if (!_IsDate1BeforeDate2(Date1, Date2))
+		{
+			swap(Date1, Date2);
+			Sign = -1;
+		}
+
+		while (_IsDate1BeforeDate2(Date1, Date2))
+		{
+			Days++;
+			Date1 = _AddOneDay(Date1);
+		}
+
+		return Days * Sign + (IncludeEndDay ? 1 : 0);
+	}
+
+	static clsDate _AddOneDay(clsDate Date)
+	{
+		short DaysInMonth = _NumberOfDaysInAMonth(Date.Month, Date.Year);
+
+		if (Date.Day == DaysInMonth)
+		{
+			if (Date.Month == 12)
+			{
+				Date.Month = 1;
+				Date.Day = 1;
+				Date.Year++;
+			}
+			else
+			{
+				Date.Month++;
+				Date.Day = 1;
+			}
+		}
+		else
+		{
+			Date.Day++;
+		}
+
+		return Date;
+	}
+
+public:
+
+	short Day;
+	short Month;
+	short Year;
+
+	enum enDateCompare { Before = -1, Equal = 0, After = 1 };
+
+	clsDate()
+	{
+		Day = 1;
+		Month = 1;
+		Year = 1900;
+	}
+
+	clsDate(short Day, short Month, short Year)
+	{
+		this->Day = Day;
+		this->Month = Month;
+		this->Year = Year;
+	}
+
+	static clsDate IncreaseDateByOneMillennium(clsDate& Date)
+	{
+		Date.Year += 1000;
+		return Date;
+	}
+
+	clsDate IncreaseDateByOneMillennium()
+	{
+		return IncreaseDateByOneMillennium(*this);
+	}
+
+	static clsDate DecreaseDateByOneDay(clsDate Date)
+	{
+		if (Date.Day == 1)
+		{
+			if (Date.Month == 1)
+			{
+				Date.Month = 12;
+				Date.Day = 31;
+				Date.Year--;
+			}
+			else
+			{
+				Date.Month--;
+				Date.Day = _NumberOfDaysInAMonth(Date.Month, Date.Year);
+			}
+		}
+		else
+		{
+			Date.Day--;
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByOneDay()
+	{
+		*this = DecreaseDateByOneDay(*this);
+	}
+
+	static clsDate DecreaseDateByOneWeek(clsDate& Date)
+	{
+		for (short i = 1; i <= 7; i++)
+		{
+			Date = DecreaseDateByOneDay(Date);
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByOneWeek()
+	{
+		DecreaseDateByOneWeek(*this);
+	}
+
+	static clsDate DecreaseDateByXWeeks(short Weeks, clsDate& Date)
+	{
+		for (short i = 1; i <= Weeks; i++)
+		{
+			Date = DecreaseDateByOneWeek(Date);
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByXWeeks(short Weeks)
+	{
+		DecreaseDateByXWeeks(Weeks, *this);
+	}
+
+	static clsDate DecreaseDateByOneMonth(clsDate& Date)
+	{
+		if (Date.Month == 1)
+		{
+			Date.Month = 12;
+			Date.Year--;
+		}
+		else
+		{
+			Date.Month--;
+		}
+
+		short DaysInCurrentMonth = _NumberOfDaysInAMonth(Date.Month, Date.Year);
+
+		if (Date.Day > DaysInCurrentMonth)
+		{
+			Date.Day = DaysInCurrentMonth;
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByOneMonth()
+	{
+		DecreaseDateByOneMonth(*this);
+	}
+
+	static clsDate DecreaseDateByXDays(short Days, clsDate& Date)
+	{
+		for (short i = 1; i <= Days; i++)
+		{
+			Date = DecreaseDateByOneDay(Date);
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByXDays(short Days)
+	{
+		DecreaseDateByXDays(Days, *this);
+	}
+
+	static clsDate DecreaseDateByXMonths(short Months, clsDate& Date)
+	{
+		for (short i = 1; i <= Months; i++)
+		{
+			Date = DecreaseDateByOneMonth(Date);
+		}
+
+		return Date;
+	}
+
+	void DecreaseDateByXMonths(short Months)
+	{
+		DecreaseDateByXMonths(Months, *this);
+	}
+
+	static clsDate DecreaseDateByOneYear(clsDate& Date)
+	{
+		Date.Year--;
+		return Date;
+	}
+
+	void DecreaseDateByOneYear()
+	{
+		DecreaseDateByOneYear(*this);
+	}
+
+	static clsDate DecreaseDateByXYears(short Years, clsDate& Date)
+	{
+		Date.Year -= Years;
+		return Date;
+	}
+
+	void DecreaseDateByXYears(short Years)
+	{
+		DecreaseDateByXYears(Years, *this);
+	}
+
+	static clsDate DecreaseDateByOneDecade(clsDate& Date)
+	{
+		Date.Year -= 10;
+		return Date;
+	}
+
+	void DecreaseDateByOneDecade()
+	{
+		DecreaseDateByOneDecade(*this);
+	}
+
+	static clsDate DecreaseDateByXDecades(short Decades, clsDate& Date)
+	{
+		Date.Year -= Decades * 10;
+		return Date;
+	}
+
+	void DecreaseDateByXDecades(short Decades)
+	{
+		DecreaseDateByXDecades(Decades, *this);
+	}
+
+	static clsDate DecreaseDateByOneCentury(clsDate& Date)
+	{
+		Date.Year -= 100;
+		return Date;
+	}
+
+	void DecreaseDateByOneCentury()
+	{
+		DecreaseDateByOneCentury(*this);
+	}
+
+	static clsDate DecreaseDateByOneMillennium(clsDate& Date)
+	{
+		Date.Year -= 1000;
+		return Date;
+	}
+
+	void DecreaseDateByOneMillennium()
+	{
+		DecreaseDateByOneMillennium(*this);
+	}
+
+	static bool IsEndOfWeek(clsDate Date)
+	{
+		return _DayOfWeekOrder(Date.Day, Date.Month, Date.Year) == 6;
+	}
+
+	bool IsEndOfWeek()
+	{
+		return IsEndOfWeek(*this);
+	}
+
+	static bool IsWeekEnd(clsDate Date)
+	{
+		short DayIndex = _DayOfWeekOrder(Date.Day, Date.Month, Date.Year);
+		return (DayIndex == 5 || DayIndex == 6);
+	}
+
+	bool IsWeekEnd()
+	{
+		return IsWeekEnd(*this);
+	}
+
+	static bool IsBusinessDay(clsDate Date)
+	{
+		return !IsWeekEnd(Date);
+	}
+
+	bool IsBusinessDay()
+	{
+		return IsBusinessDay(*this);
+	}
+
+	static short DaysUntilTheEndOfWeek(clsDate Date)
+	{
+		return 6 - _DayOfWeekOrder(Date.Day, Date.Month, Date.Year);
+	}
+
+	short DaysUntilTheEndOfWeek()
+	{
+		return DaysUntilTheEndOfWeek(*this);
+	}
+
+	static short DaysUntilTheEndOfMonth(clsDate Date1)
+	{
+		clsDate EndOfMonthDate;
+		EndOfMonthDate.Day = _NumberOfDaysInAMonth(Date1.Month, Date1.Year);
+		EndOfMonthDate.Month = Date1.Month;
+		EndOfMonthDate.Year = Date1.Year;
+
+		return _GetDifferenceInDays(Date1, EndOfMonthDate, true);
+	}
+
+	short DaysUntilTheEndOfMonth()
+	{
+		return DaysUntilTheEndOfMonth(*this);
+	}
+
+	static short DaysUntilTheEndOfYear(clsDate Date1)
+	{
+		clsDate EndOfYearDate;
+		EndOfYearDate.Day = 31;
+		EndOfYearDate.Month = 12;
+		EndOfYearDate.Year = Date1.Year;
+
+		return _GetDifferenceInDays(Date1, EndOfYearDate, true);
+	}
+
+	short DaysUntilTheEndOfYear()
+	{
+		return DaysUntilTheEndOfYear(*this);
+	}
+
+	static short CalculateBusinessDays(clsDate DateFrom, clsDate DateTo)
+	{
+		short Days = 0;
+
+		while (_IsDate1BeforeDate2(DateFrom, DateTo))
+		{
+			if (IsBusinessDay(DateFrom))
+				Days++;
+
+			DateFrom = _AddOneDay(DateFrom);
+		}
+
+		return Days;
+	}
+
+	static short CalculateVacationDays(clsDate DateFrom, clsDate DateTo)
+	{
+		return CalculateBusinessDays(DateFrom, DateTo);
+	}
+
+	static clsDate CalculateVacationReturnDate(clsDate DateFrom, short VacationDays)
+	{
+		short WeekEndCounter = 0;
+
+		for (short i = 1; i <= VacationDays; i++)
+		{
+			if (IsWeekEnd(DateFrom))
+				WeekEndCounter++;
+
+			DateFrom = _AddOneDay(DateFrom);
+		}
+
+		for (short i = 1; i <= WeekEndCounter; i++)
+		{
+			DateFrom = _AddOneDay(DateFrom);
+		}
+
+		return DateFrom;
+	}
+
+	static bool IsDate1AfterDate2(clsDate Date1, clsDate Date2)
+	{
+		return (!_IsDate1BeforeDate2(Date1, Date2) && !_IsDate1EqualDate2(Date1, Date2));
+	}
+
+	bool IsDateAfterDate2(clsDate Date2)
+	{
+		return IsDate1AfterDate2(*this, Date2);
+	}
+
+	static enDateCompare CompareDates(clsDate Date1, clsDate Date2)
+	{
+		if (_IsDate1BeforeDate2(Date1, Date2))
+			return enDateCompare::Before;
+
+		if (_IsDate1EqualDate2(Date1, Date2))
+			return enDateCompare::Equal;
+
+		return enDateCompare::After;
+	}
+
+	enDateCompare CompareDates(clsDate Date2)
+	{
+		return CompareDates(*this, Date2);
+	}
 };
